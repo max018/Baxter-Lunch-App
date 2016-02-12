712e05532a8d5b51981ceee662fb4b66cb20b999
Rob Korobkin <rob.korobkin@gmail.com>
2016-02-12 11:23:24 -0500

    js start to public app

--- frontend/Public/baxter-public.js
+++ frontend/Public/baxter-public.js
@@ -8,6 +8,9 @@ var app = angular.module('baxterApp', []);
 app.controller('baxterCtrl', ['$scope',
 	function($scope){
 
+		$scope.openOrderForm = function(){
+			$scope.view = 'orderForm';
+		}
 
 		
 	}
