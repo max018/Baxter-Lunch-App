5571c1a317f456dc978c034dc7f66a718de5f54f
Rob Korobkin <rob.korobkin@gmail.com>
2016-02-26 13:18:00 -0500

    progress on js tutorial

---
+++ -workspace/js tutorial/script.js
@@ -0,0 +1,31 @@
+var days = [
+	{	"holiday": null, 
+		"order": {
+			"restaurant": "Zen Chinese Bistro",
+			"option": "Pork Fried Rice"
+		}
+	},
+
+	{	"holiday": "New Year's Eve", 
+		"order": null
+	},
+
+	{	"holiday": "New Year's Day", 
+		"order": null
+	},
+
+	{	"holiday": null, 
+		"order": {
+			"restaurant": "Portland Pie",
+			"option": "Pizza: Cheese, Cheese"
+		}
+	},	
+
+	{	"holiday": null, 
+		"order": null
+	},	
+];
+
+console.log(days)
+
+
