e93ea7956cfef0df8f3983bb99a3db32080a4fed
Max Coury <maximillian.coury@baxter-academy.org>
2016-02-26 10:40:28 -0500

    Stage 3 of Office UI tables

--- frontend/Admin/baxter.js
+++ frontend/Admin/baxter.js
@@ -8,7 +8,9 @@ var app = angular.module('baxterApp', []);
 app.controller('baxterCtrl', ['$scope',
 	function($scope){
 
-
+		$scope.loadView = function(viewName){
+			$scope.view = viewName;
+		}
 		
 	}
 ]);
